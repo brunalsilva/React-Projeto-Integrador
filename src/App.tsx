3775942e7977564911371f0291ec37c55cd457a0
kayque <kayquegps2@gmail.com>
2022-01-19 09:24:27 -0300

    inicialização do projeto e adição das dependencias core e icons do material ui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Dandara will be the best project imo</h1>
+    </>
+  );
+}
+
+export default App;
