4c105261be529195550a736d594279558d1d0ac7
devlarissa <devlarissasilva@gmail.com>
2022-01-21 14:47:25 -0300

    Adicionando componente appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/static/navbar/Navbar';
 
 function App() {
   return (
     <>
-    <h1>Dandara will be the best project imo</h1>
+    <Navbar />
     </>
   );
 }
