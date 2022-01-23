6c775c2ce7257a92b1bc5f00733be0c22c0b5597
Bruna Silva <beyss21@gmail.com>
2022-01-23 15:25:42 -0300

    criacao paginas fale conosco e sobre nos

---
+++ src/paginas/FaleConosco/FaleConosco.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography, Box, Grid } from '@material-ui/core';
+import './FaleConosco.css';
+
+function FaleConosco() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default FaleConosco;
---
+++ src/paginas/SobreNos/SobreNos.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography, Box, Grid } from '@material-ui/core';
+import './SobreNos.css';
+
+function SobreNos() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default SobreNos;
