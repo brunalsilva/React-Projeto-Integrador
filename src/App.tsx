12bb1a1aa4037c992ae2c9a8f193d26d8d2c3810
kayque <kayquegps2@gmail.com>
2022-01-26 15:55:15 -0300

    adicionando post,put e delete para as categorias e para os produtos

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import './App.css';
 import Navbar from './components/static/navbar/Navbar';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaCategoria from './components/categorias/listacategoria/ListaCategoria';
+import ListaProduto from './components/produtos/listaproduto/ListaProduto';
+import CadastroProd from './components/produtos/cadastroProd/CadastroProd';
+import CadastroCategoria from './components/categorias/cadastroCategoria/CadastroCategoria';
+import DeletarCategoria from './components/categorias/deleterCategoria/DeletarCategoria';
 
 
 function App() {
@@ -32,6 +37,28 @@ function App() {
             <Route path='/cadastrousuario'>
               <CadastroUsuario />
             </Route>
+
+            <Route path='/categorias'>
+            <ListaCategoria />
+          </Route>
+          <Route path='/produtos'>
+            <ListaProduto />
+          </Route>
+          <Route exact path='/formularioProduto'>
+            <CadastroProd />
+          </Route>
+          <Route exact path='/formularioProduto/:id'>
+            <CadastroProd />
+          </Route>
+          <Route exact path='/formularioCategoria'>
+            <CadastroCategoria />
+          </Route>
+          <Route exact path='/formularioCategoria/:id'>
+            <CadastroCategoria />
+          </Route>
+          <Route path='/deletarCategoria/:id'>
+            <DeletarCategoria />
+          </Route>
           </div>
         </Switch>
         <Footer />
---
+++ src/models/Categoria.ts
@@ -0,0 +1,8 @@
+interface Categoria{
+    id?: number;
+    nome: string;
+    descricao: string;
+    palavraChave: string;
+}
+
+export default Categoria;
