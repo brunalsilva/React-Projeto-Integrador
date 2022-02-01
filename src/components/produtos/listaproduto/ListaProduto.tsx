1a778944ea6d9655d4abe976fa9376b32b31f4fa
kayque <kayquegps2@gmail.com>
2022-02-01 16:34:55 -0300

    alterando botoes do produto de acordo com o usuario

--- src/components/produtos/listaproduto/ListaProduto.tsx
+++ src/components/produtos/listaproduto/ListaProduto.tsx
@@ -10,9 +10,12 @@ import { CardMedia, Card} from '@mui/material';
 
 
 
+
+
 function ListaProduto() {
     const [produtos, setProdutos] = useState<Produto[]>([])
     const [token, setToken] = useLocalStorage('token');
+    const [idUser, setIdUser] = useLocalStorage('id');
     
     let history = useHistory();
   
@@ -69,6 +72,49 @@ function ListaProduto() {
                     <Link to={`/formularioProduto/${produto.id}`} className="text-decorator-none" >
                       <Box mx={1}>
                         <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                          Comprar
+                        </Button>
+                      </Box>
+                    </Link>
+                    <Link to={`/deletarProduto/${produto.id}`} className="text-decorator-none">
+                      <Box mx={1}>
+                        <Button variant="contained" size='small' color="secondary">
+                       Comprar
+                        </Button>
+                      </Box>
+                    </Link>
+                  </Box>
+                </CardActions>
+              </Card>
+            </Box>
+          ):(
+            <Box m={2} >
+              <Card variant="outlined" sx={{maxWidth:345}}>
+                <CardMedia component="img" height="194" image={produto.foto} alt={produto.nome}/>
+                <CardContent>
+                  <Typography color="textSecondary" gutterBottom>
+                    {produto.servico.toString()=="true"?"Produto":""}
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                    {produto.nome}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {produto.descricao}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {produto.categoria?.descricao}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    <b>R$ {produto.preco}</b>
+                  </Typography>
+                  <Typography variant="body2" component="p"></Typography>
+                </CardContent>
+                <CardActions>
+                  <Box display="flex" justifyContent="center" mb={1.5}>
+  
+                    <Link to={`/formularioProduto/${produto.id}`} className="text-decorator-none" >
+                      <Box mx={1}>
+                        <Button variant="contained" className="marginLeft" size='small' color="primary" >
                           atualizar
                         </Button>
                       </Box>
@@ -84,7 +130,7 @@ function ListaProduto() {
                 </CardActions>
               </Card>
             </Box>
-          ):'')
+          ))
         }
       </>
     )
