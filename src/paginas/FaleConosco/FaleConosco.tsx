a65856018ab495f6031bb0b05a1b100babb5d902
Bruna Silva <beyss21@gmail.com>
2022-02-01 16:48:17 -0300

    alteracao fale conosco

--- src/paginas/FaleConosco/FaleConosco.tsx
+++ src/paginas/FaleConosco/FaleConosco.tsx
@@ -1,5 +1,7 @@
 import emailjs from "emailjs-com";
 import './FaleConosco.css';
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 
 export default function FaleConosco() {
 
@@ -10,28 +12,34 @@ export default function FaleConosco() {
      }
      return (
           <>
-               <div>
-                    <h2>Contato</h2>
-                    <form onSubmit={sendEmail}>
-                         <div>
+               <Grid container direction='row' justifyContent='center' alignItems='center'>
+                    <Grid alignItems='center' xs={6}>
+                         <Box className='box-fundo-opaca'>
                               <div>
-                                   <label>Nome</label>
-                                   <input type="text" autoFocus className="form-control" required placeholder="Nome" name="name" />
+                                   <h2>Contato</h2>
+                                   <form onSubmit={sendEmail}>
+                                        <div>
+                                             <div>
+                                                  <label>Nome</label>
+                                                  <input type="text" autoFocus className="form-control" required placeholder="Nome" name="name" />
+                                             </div>
+                                             <div >
+                                                  <label>Email</label>
+                                                  <input type="email" required placeholder="Seu email" name="email" />
+                                             </div>
+                                             <div>
+                                                  <label>Mensagem</label>
+                                                  <textarea required placeholder="Sua mensagem" name="message"></textarea>
+                                             </div>
+                                             <div>
+                                                  <input type="submit" value="Enviar mensagem"></input>
+                                             </div>
+                                        </div>
+                                   </form>
                               </div>
-                              <div >
-                                   <label>Email</label>
-                                   <input type="email" required placeholder="Seu email" name="email" />
-                              </div>
-                              <div>
-                                   <label>Mensagem</label>
-                                   <textarea required placeholder="Sua mensagem" name="message"></textarea>
-                              </div>
-                              <div>
-                                   <input type="submit" value="Enviar mensagem"></input>
-                              </div>
-                         </div>
-                    </form>
-               </div>
+                         </Box>
+                    </Grid>
+               </Grid>
           </>
      )
 }
